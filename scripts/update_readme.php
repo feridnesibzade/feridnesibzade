ce90f241ff2278e82110f8eb76b7df542b8956cc
Farid <artisan.farid@gmail.com>
2025-07-31 13:22:08 +0400

    Update update_readme.php

--- scripts/update_readme.php
+++ scripts/update_readme.php
@@ -1,13 +1,13 @@
 <?php
 
-$path = __DIR__ . '/../README.md';
+$readme = __DIR__ . '/../README.md';
 $path = 'https://github.com/feridnesibzade/feridnesibzade/blob/main/assets/wallpapers/';
-if (!file_exists($path)) {
+if (!file_exists($readme)) {
     fwrite(STDERR, "README.md not found\n");
     exit(1);
 }
 
-$markdown = file_get_contents($path);
+$markdown = file_get_contents($readme);
 $stamp    = gmdate('Y-m-d H:i') . ' UTC';
 $type = array_rand(['.jpg', '.gif']);
 $fileName = rand(1,2);
@@ -17,6 +17,6 @@ $updated = preg_replace(
     $markdown
 );
 
-file_put_contents($path, $updated);
+file_put_contents($readme, $updated);
 
 // ![Dark Souls](https://github.com/feridnesibzade/feridnesibzade/blob/main/assets/06a85b703ccc50fcc2214bac56214f48.gif)
